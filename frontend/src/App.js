799ec667a9e3d3660fd8ab2db7473ce0fab88a40
Josh Motsenbocker <joshmotsenbocker@gmail.com>
2022-10-24 20:05:37 -0400

    frontend ground work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,105 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
-const App = () => {
-  const [users, setUsers] = useState([])
-  const getData = async() => {
-    const res = await axios.get('/api/mintNFT')
-    setUsers(res.data)
-    console.log(res)
-  }
 
-  useEffect(() => {
-    getData()
-  }, [])
- 
+
+const App = () => {
+
+const [account, setAccount] = useState("")
+
+const handleChange = (event) => {
+
+
+  setAccount(event.target.name)
+
+}
   return (
-    <div>
-      <h1>Hello</h1>
+<div>
+
+<div style={{color: "white"}}>
+Find An Event | <br></br>
+Collect Event NFT 
+</div>
+<div class="login-box">
+  <h2>Event Token Generator</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required="" />
+      <label>Account Number</label>
     </div>
-  )
+    <div class="user-box">
+      <input type="text" name="" required="" />
+      <label>Secret </label>
+    </div>
+    <div class="user-box">
+      <input type="number" name="" required="" />
+      <label>Flag Value </label>
+    </div>
+    <div class="user-box">
+      <input type="number" name="" required="" value="0" />
+      <label># of Attendees </label>
+    </div>
+    <div class="user-box">
+      <input type="text" name="" required="" />
+      <label>Image URI </label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
+</div>
+ 
+</div>
+ 
+ )
 }
 // {users.map(u => <h4 key={u._id}>userName : {u.userName}</h4>)}
+
+/*
+
+<html>
+    
+    <label for="fname">Account Number:</label>
+    <input type="text" name="account" onChange={handleChange} />
+    {account}
+  
+    </html>
+
+
+const [inputs, setInputs] = useState([])
+
+const handleRegistration = (data) => console.log(data);
+const { register, handleSubmit } = useForm();
+
+
+const handleChange = (event) => {
+  const account = event.target.account;
+  const flag = event.target.flag;
+  const value = event.target.value;
+  setInputs(values => ({account,flag,value}))
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(inputs);
+}
+
+
+const getData = async() => {
+  const res = await axios.get('/api/mintNFT')
+  setInputs(res.data)
+  console.log(res)
+}
+
+
+
+useEffect(() => {
+  getData()
+}, [])
+ */
 export default App
 
