7b27f41426098bba48980d97a84fe34cbb808c45
Josh Motsenbocker <joshmotsenbocker@gmail.com>
2022-10-26 08:27:35 -0400

    fixed forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,19 @@ const [account, setAccount] = useState("")
 const [secret, setSecret] = useState("")
 const [numberOfTokens, setNumberOfTokens] = useState(0)
 const [response, setResponse] = useState("")
+const [radioButton, setRadioButton] = useState("mint")
 
-const [radioButton, setRadioButton] = useState("mintNFT")
+//metadata:
+const [date, setDate] = useState("")
+const [location, setlocation] = useState("")
+const [description, setdescription] = useState("")
+const [image, setimage] = useState("")
+const [offer, setoffer] = useState("")
+const [title, settitle] = useState("")
+const [time, settime] = useState("")
+const [qrCode, setqrCode] = useState("")
+const [uniqueTaxon, setuniqueTaxon] = useState("")
+
 
 const handleAccountChange = (event) => {
 
@@ -33,12 +44,72 @@ const handleNumberOfTokensChange = (event) => {
 
 }
 
+const handleMetaChange = (event) => {
+
+  if(event.target.name == 'date'){
+    setDate(event.target.value)
+  }
+  if(event.target.name == 'location'){
+    setlocation(event.target.value)
+  }
+  if(event.target.name == 'description'){
+    setdescription(event.target.value)
+  }
+  if(event.target.name == 'image'){
+    setimage(event.target.value)
+  }
+  if(event.target.name == 'offer'){
+    setoffer(event.target.value)
+  }
+  if(event.target.name == 'title'){
+    settitle(event.target.value)
+  }
+  if(event.target.name == 'time'){
+    settime(event.target.value)
+  }
+  if(event.target.name == 'qrCode'){
+    setqrCode(event.target.value)
+  }
+  if(event.target.name == 'uniqueTaxon'){
+    setuniqueTaxon(event.target.value)
+  }
+
+}
+
+const handleRadioButtonChange = (event) => {
+  console.log(event.target.id)
+  if(event.target.id === 'burn' && event.target.checked){
+    setRadioButton("burn")
+  }
+  if(event.target.id === 'burnAll' && event.target.checked){
+    setRadioButton("burnAll")
+  }
+  if(event.target.id === 'mint' && event.target.checked){
+    setRadioButton("mint")
+  }
+  
+  
+
+
+}
+
 const handleSubmit = async() => {
  
   let formData = {
     account: account, 
     secret: secret, 
-    numberOfTokens: numberOfTokens
+    numberOfTokens: numberOfTokens,
+    date: date,
+   location: location,
+   description: description,
+   image: image,
+   numberOfTokens: numberOfTokens,
+   offer: offer,
+   title: title,
+   time: time,
+   qrCode: true,
+   uniqueTaxon: true 
+
   }
 
   
@@ -53,9 +124,9 @@ const handleSubmit = async() => {
 
 
 
-/*
+
 const getData = async() => {
-  const res = await axios.get('/api/mintNFT')
+  const res = await axios.get('/api/burnAllNFT')
  // setAccount(res.data)
   console.log(res)
 }
@@ -65,13 +136,13 @@ const getData = async() => {
 useEffect(() => {
   getData()
 }, [])
-*/
+
 
 const formGenerator = () => {
   
 
   
-if(radioButton == "mintNFT"){
+if(radioButton == "mint"){
   return(
     <form>
 
@@ -92,34 +163,34 @@ if(radioButton == "mintNFT"){
     </div>
     <h2>Metadata (optional)</h2>
     <div class="user-box">    
-      <input type="float" name="" required="" />
+      <input type="float" name="offer" required="" onChange={handleMetaChange}/>
       <label>Initial Sell Offer</label>
     </div>
 
     <div class="user-box">    
-      <input type="text" name="" required="" />
+      <input type="text" name="title" required="" onChange={handleMetaChange}/>
       <label>Event Title</label>
     </div>
 
     <div class="user-box">    
-      <input type="file" name="" required="" />
+      <input type="file" name="image" required="" onChange={handleMetaChange}/>
       <label>*Image File*</label>
     </div>
 
 
     
     <div class="user-box">
-      <input type="number" name="" required="" value="0" />
+      <input type="date" name="date" required="" onChange={handleMetaChange} />
       <label>Date of Event </label>
     </div>
 
     <div class="user-box">
-      <input type="number" name="" required="" value="0" />
+      <input type="text" name="location" required="" onChange={handleMetaChange} />
       <label>Location</label>
     </div>
 
     <div class="user-box">    
-      <input type="text" name="" required="" />
+      <input type="text" name="time" required="" />
       <label>Time</label>
     </div>
 
@@ -145,6 +216,69 @@ if(radioButton == "mintNFT"){
   </form>
   )
   }
+
+
+  if(radioButton == "burn"){
+    return(
+    <form>   
+      
+  <div class="user-box">
+    <input type="text" name="" required="" onChange={handleAccountChange} />
+    <label>Account Number</label>
+  </div>
+
+
+  <div class="user-box">
+    <input type="text" name="" required="" onChange={handleSecretChange}/>
+    <label>Secret </label>
+  </div>
+
+  <div class="user-box">
+    <input type="number" name="" required="" onChange={handleNumberOfTokensChange} />
+    <label># of Event tickets / tokens to generate</label>
+  </div>
+
+
+  <a onClick={handleSubmit} href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+
+  </form>
+  )
+ 
+  }
+
+  if(radioButton == "burnAll"){
+return(
+    <form>   
+      
+  <div class="user-box">
+    <input type="text" name="" required="" onChange={handleAccountChange} />
+    <label>Account Number</label>
+  </div>
+
+
+  <div class="user-box">
+    <input type="text" name="" required="" onChange={handleSecretChange}/>
+    <label>Secret </label>
+  </div>
+  <a onClick={handleSubmit} href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  
+  </form>
+)
+
+}  
+
 }
 
 
@@ -165,28 +299,31 @@ if(radioButton == "mintNFT"){
 
 <input
                 type="radio"
-                name="address"
+                name="formOption"
+                id="burn"
            //     value={result.ADDRESS}
             //    checked={this.state.address === result.ADDRESS}
-            //    onChange={this.onAddressChanged}
+                 onChange={handleRadioButtonChange}
 ></input>Burn NFT
 <br></br>
 
   <input
                 type="radio"
-                name="address"
+                name="formOption"
+                id="burnAll"
            //     value={result.ADDRESS}
             //    checked={this.state.address === result.ADDRESS}
-            //    onChange={this.onAddressChanged}
+            onChange={handleRadioButtonChange}
 ></input>Burn All NFT's
 <br></br>
 
   <input
                 type="radio"
-                name="address"
+                name="formOption"
+                id="mint"
            //     value={result.ADDRESS}
             //    checked={this.state.address === result.ADDRESS}
-            //    onChange={this.onAddressChanged}
+            onChange={handleRadioButtonChange}
 ></input>Mint Event NFT's
 
 <br></br>
